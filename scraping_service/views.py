4607554dff26718f2c393e4afbca9a6194155b38
Vladislav.H <vkhrabrov94@gmail.com>
2022-10-09 19:59:11 +0300

    first

---
+++ scraping_service/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+import datetime
+
+def home(request):
+	date = datetime.datetime.now().date()
+	name = 'Donalnd'
+	_context = {
+		'date': date,
+		'name': name
+	}
+	return render(request, 'home.html', _context)
+
